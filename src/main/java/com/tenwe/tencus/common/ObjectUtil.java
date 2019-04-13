3821407a948e7e9138171aa6f9bd75381c738858
jeftom <163pc@163.com>
2019-04-14 04:12:57 +0800

    init repository

---
+++ src/main/java/com/tenwe/tencus/common/ObjectUtil.java
@@ -0,0 +1,11 @@
+package com.tenwe.tencus.common;
+
+/**
+ * class description
+ *
+ * @author jeftom <jeftom@tenwe.com>
+ * @date 2019-04-14 04:11
+ * @since 1.0.0
+ */
+public class ObjectUtil {
+}
